9aab383fff97d735e7418d48d914a2312a6a5b62
Artem <artem.martiukhin@ambisafe.com>
2024-01-02 02:01:57 -0800

    feat: home page

--- packages/frontend/src/components/Header/Header.tsx
+++ packages/frontend/src/components/Header/Header.tsx
@@ -5,11 +5,12 @@ import { useAuth } from "../../contexts/AuthContext";
 import logo from "../../assets/images/logo.png";
 import eng from "../../assets/images/eng.png";
 import drop from "../../assets/images/drop.png";
+import { ButtonL } from "../../props/ButtonL";
 
 import axios from "axios";
 
 const Header = () => {
-  const { user, login } = useAuth(); 
+  const { user, login } = useAuth();
 
   const fetchUserData = async () => {
     try {
@@ -45,7 +46,7 @@ const Header = () => {
           <img
             loading="lazy"
             src={logo}
-            className="mb-2 aspect-[4] object-contain object-center w-[150px] overflow-hidden shrink-0 max-w-full"
+            className="mb-2 aspect-[4] object-contain object-center w-[175px] overflow-hidden shrink-0 max-w-full"
           />
           <div className="items-stretch self-center flex justify-between gap-5 my-auto">
             <div className="text-sky-950 text-base font-bold leading-6">
@@ -72,15 +73,10 @@ const Header = () => {
         </div>
         <div className="items-stretch self-stretch flex gap-3.5 max-md:max-w-full max-md:flex-wrap">
           <div className="items-stretch bg-white flex justify-between gap-3 px-4 py-3 rounded-[46px] max-md:pr-5"></div>
-          <div className="text-white text-base font-bold leading-6 whitespace-nowrap justify-center items-stretch bg-sky-950 grow px-7 py-2.5 rounded-[57px] max-md:px-5">
-            {user ? (
-              <span>Welcome, {user.firstName}! </span>
-            ) : (
-              <button onClick={handleLogin}>Login with Google</button>
-            )}
-          </div>
+
           <div className="justify-between flex gap-1 px-2 py-2.5 items-start">
-            <div className="flex gap-1.5 items-start">
+            <div className="flex mt-1 gap-1.5 items-start">
+
               <img
                 loading="lazy"
                 src={eng}
@@ -96,6 +92,22 @@ const Header = () => {
               className="aspect-square object-contain object-center w-6 overflow-hidden self-stretch shrink-0 max-w-full"
             />
           </div>
+        </div>
+          <div className="flex gap-1.5 items-start">
+              {user ? (
+                <div className="text-sky-950 text-base font-bold leading-6">
+                  <span>Welcome, {user.firstName}! </span>
+                </div>
+              ) : (
+                <ButtonL
+                  iconLeft={false}
+                  iconOnly={false}
+                  iconRight={false}
+                  text="Login"
+                  buttonType="default"
+                  onClick={handleLogin}
+                />
+              )}
         </div>
       </div>
     </div>
--- packages/frontend/src/pages/Home/Home.tsx
+++ packages/frontend/src/pages/Home/Home.tsx
@@ -1,14 +1,66 @@
 // src/pages/Home/Home.tsx
-import React from 'react';
-import styles from './Home.module.css';
+import React from "react";
+import { ButtonL } from "../../props/ButtonL";
+import { Link } from "react-router-dom";
+
+import main from "../../assets/images/main.png";
 
 const Home: React.FC = () => {
   return (
-    <div className={styles.home}>
-      <h1>Welcome to Our Dog Kennel</h1>
-      <p>Find the best companions and friends here!</p>
+    <div className="flex justify-center items-center">
+      <div className="relative w-[1620px] h-[695px] bg-white rounded-[0px_0px_40px_40px] overflow-hidden [background:linear-gradient(180deg,rgb(251.81,238.23,212.99)_6.17%,rgb(251.81,238.23,212.99)_75.14%,rgb(255,230.83,185.94)_100%)]">
+        <div className="top-[84px] left-[-91px] absolute w-[1531px] h-[1360px]">
+          <div className="top-0 left-0 absolute w-[1531px] h-[1360px]">
+            <div className="absolute w-[635px] h-[635px] top-[231px] left-[734px] bg-[#f7dba7] rounded-[99px] rotate-[9.35deg]" />
+            <div className="absolute w-[635px] h-[635px] top-[201px] left-[791px] bg-[#00172d] rounded-[99px] rotate-[25.23deg]" />
+            <div className="absolute w-[635px] h-[635px] top-[603px] left-[123px] bg-[#00172d] rounded-[99px] rotate-[56.47deg] opacity-40" />
+            <div className="absolute w-[67px] h-[67px] top-[90px] left-[214px] bg-secondary-colormon-yellow rounded-[20px] rotate-[25.23deg]" />
+            <div className="absolute w-[15px] h-[15px] top-[66px] left-[850px] bg-[#f7dba7] rounded-[4px] rotate-[20.79deg]" />
+            <div className="absolute w-[27px] h-[27px] top-[120px] left-[823px] bg-[#f7dba7] rounded-[9px] rotate-[-22.85deg]" />
+            <div className="absolute w-[21px] h-[21px] top-[129px] left-[822px] bg-[#00172d] rounded-[6px] rotate-[-43.00deg]" />
+            <img
+              className="absolute w-[910px] h-[611px] top-[0px] left-[621px]"
+              alt="Good humored woman"
+              src={main}
+            />
+            <div className="absolute top-[95px] left-[221px] [font-family:'SVN-Gilroy-XBold',Helvetica] font-bold text-primary-colordark-blue-80 text-[60px] tracking-[0] leading-[68px] whitespace-nowrap">
+              Nevlemar
+            </div>
+            <div className="absolute top-[167px] left-[221px] [font-family:'SVN-Gilroy-Bold',Helvetica] font-bold text-primary-colordark-blue-80 text-[46px] tracking-[0] leading-[60px] whitespace-nowrap">
+              dog kennel
+            </div>
+            <p className="absolute w-[480px] top-[251px] left-[221px] font-body-16px-medium font-[number:var(--body-16px-medium-font-weight)] text-neutral-color80 text-[length:var(--body-16px-medium-font-size)] tracking-[var(--body-16px-medium-letter-spacing)] leading-[var(--body-16px-medium-line-height)] [font-style:var(--body-16px-medium-font-style)]">
+              Having a pet means you have more joy, a new friend, a happy person
+              who will always be with you to have fun. We have 200+ different
+              pets that can meet your needs!
+            </p>
+          </div>
+          <div className="flex justify-center space-x-4">
+            <Link to="/males">
+              <ButtonL
+                style={{ marginLeft: "32px" }}
+                className="!absolute !left-[402px] !top-[358px]"
+                iconLeft={false}
+                iconOnly={false}
+                iconRight={false}
+                text="Explore Now"
+                buttonType="default"
+              />
+            </Link>
+
+            <ButtonL
+              className="!absolute !left-[221px] !top-[358px]"
+              iconLeft={false}
+              iconOnly={false}
+              iconRight
+              text="About Us"
+              buttonType="outline"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default Home;
